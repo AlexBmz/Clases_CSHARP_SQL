ee1e941e28f5f2f0755e8c53fb7c3b1a93bdbb63
Jordan <blasdta@gmail.com>
2020-09-20 13:27:17 -0500

    Pregunta2

---
+++ Practica/Practica1.CosolApp1JABM/ClienteDAO.cs
@@ -0,0 +1,89 @@
+﻿using Practica1.CosolApp1JABM.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1.CosolApp1JABM
+{
+    public class ClienteDAO
+    {
+        public void RegistrarCliente()
+        {
+            Console.Write("Ingrese el  nombre Cliente : ");
+            string _nombreCli = Console.ReadLine();
+            Console.Write("Ingrese apellido del Cliente");
+            string _apeCliente = Console.ReadLine();
+            Console.Write("Ingrese la Fecha de Nacimiento : ");
+            DateTime _fechaNac = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Ingrese el Dni del Cliente : ");
+            string _Dni = Console.ReadLine();
+            tbCliente oTbCli = new tbCliente()
+            {
+                nombreCliente = _nombreCli,
+                apellidosCliente = _apeCliente,
+                fechaNacimiento = _fechaNac,
+                dniCliente = _Dni
+
+            };
+
+
+            using (var conn = new BD_Practica1EF())
+            {
+                conn.tbCliente.Add(oTbCli);
+                conn.SaveChanges();
+            }
+
+            Console.WriteLine("Registro correctamente");
+
+
+        }
+
+        public void ListarCliente()
+        {
+
+            List<tbCliente> listaCli = new List<tbCliente>();
+
+            using (var conn = new BD_Practica1EF())
+            {
+                //Obtener la información desde EF
+                listaCli = conn.tbCliente.ToList();
+            }
+            for (int i = 0; i < listaCli.Count; i++)
+            {
+                Console.WriteLine(listaCli[i].idCliente + "-" + listaCli[i].nombreCliente
+                    + "-" + listaCli[i].apellidosCliente + "-" + listaCli[i].fechaNacimiento
+                    + "-" + listaCli[i].dniCliente);
+            }
+
+
+        }
+
+        public void ActualizarCliente()
+        {
+            Console.Write("Ingrese el Id del Cliente a Actualizar : ");
+            int idCli = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el Nombre nuevo  del Cliente a Actualizar  : ");
+            string nombreCli = Console.ReadLine();
+            Console.Write("Ingrese el Apellido nuevo  del Cliente a Actualizar  : ");
+            string apeCli = Console.ReadLine();
+            Console.Write("Ingrese la fecha de Nacimiento a Actualizar :");
+            DateTime fechaCli = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Ingrese el Dni Del cliente a Actualizar : ");
+            String DniCli = Console.ReadLine();
+
+            using (var conn = new BD_Practica1EF())
+            {
+                tbCliente cli = conn.tbCliente.Find(idCli);
+                cli.nombreCliente = nombreCli;
+                cli.apellidosCliente = apeCli;
+                cli.fechaNacimiento = fechaCli;
+                cli.dniCliente = DniCli;
+                conn.SaveChanges();
+            }
+
+        }
+    }
+}
---
+++ Practica/Practica1.CosolApp1JABM/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1.CosolApp1JABM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rsp = "";
+            int opc2;
+            int opc3;
+            string rsp2 = "";
+            String rsp3 = "";
+            string rsp4 = "";
+            ClienteDAO oClienteDAO = new ClienteDAO();
+            do
+            {
+                Console.WriteLine("1 - Registrar ");
+                Console.WriteLine("2 - Listar ");
+                Console.WriteLine("3 - Actualizar");
+                Console.WriteLine("\n Ingrese una Opcion");
+                int opc = int.Parse(Console.ReadLine());
+
+                switch (opc)
+                {
+                    case 1:
+                        {
+                            do
+                            {
+                                Console.WriteLine("1 - Registrar Cliente ");
+                                Console.WriteLine("2 - Registrar Tarjeta ");
+                                Console.WriteLine("3 - registrar Pais");
+                                Console.Write("\n Ingrese una Opcion : ");
+                                opc2 = int.Parse(Console.ReadLine());
+                                switch (opc)
+                                {
+                                    case 1:
+                                        {
+                                            oClienteDAO.RegistrarCliente();
+                                        }
+                                        break;
+                                    case 2:
+                                        {
+
+                                        }
+                                        break;
+                                    case 3:
+                                        {
+
+                                        }
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                Console.WriteLine("Desea Regresar al menu de Registro ? : (S/N)");
+                                rsp2 = Console.ReadLine();
+                                Console.WriteLine();
+
+
+                            } while (rsp == "s" || rsp == "S");
+
+                        }
+                        break;
+
+                    case 2:
+                        {
+                            do
+                            {
+                                Console.WriteLine("1 - Listar Cliente ");
+                                Console.WriteLine("2 - Listar Tarjeta ");
+                                Console.WriteLine("3 - Listar Pais");
+                                Console.Write("\n Ingrese una Opcion : ");
+                                opc2 = int.Parse(Console.ReadLine());
+                                switch (opc2)
+                                {
+                                    case 1:
+                                        {
+                                            oClienteDAO.ListarCliente();
+                                        }
+                                        break;
+                                    case 2:
+                                        {
+
+                                        }
+                                        break;
+                                    case 3:
+                                        {
+
+                                        }
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                Console.WriteLine("Desea Regresar al menu de Listado ? : (S/N)");
+                                rsp3 = Console.ReadLine();
+                                Console.WriteLine();
+
+                            } while ((rsp3 == "s" || rsp3 == "S"));
+                        }
+                
+                        break;
+
+                    case 3:
+                        {
+                            do
+                            {
+                                Console.WriteLine("1 - Actualizar Cliente ");
+                                Console.WriteLine("2 - Actualizar Tarjeta ");
+                                Console.WriteLine("3 - Actualizar Pais");
+                                Console.Write("\n Ingrese una Opcion : ");
+                                opc3 = int.Parse(Console.ReadLine());
+                                switch (opc3)
+                                {
+                                    case 1:
+                                        {
+                                            oClienteDAO.ActualizarCliente();
+                                        }
+                                        break;
+                                    case 2:
+                                        {
+
+                                        }
+                                        break;
+                                    case 3:
+                                        {
+
+                                        }
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                Console.WriteLine("Desea Regresar al menu de Actualizado ? : (S/N)");
+                                rsp4= Console.ReadLine();
+                                Console.WriteLine();
+
+                            } while ((rsp4 == "s" || rsp4 == "S"));
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+
+                Console.Write("Desea regresar al menu principal ?(S/N)  :");
+                rsp = Console.ReadLine();
+
+
+            } while (rsp=="s"|| rsp=="S");
+        }
+    }
+}
